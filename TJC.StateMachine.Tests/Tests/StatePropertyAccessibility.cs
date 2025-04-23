10ed2e5eb650416abf612bd5d532c12853068c67
TylerCarrol <25536704+TylerCarrol@users.noreply.github.com>
2025-04-23 01:13:31 +0000

    🧹 chore: [MegaLinter] Apply [1] automatic fixes

--- TJC.StateMachine.Tests/Tests/StatePropertyAccessibility.cs
+++ TJC.StateMachine.Tests/Tests/StatePropertyAccessibility.cs
@@ -9,14 +9,20 @@ namespace TJC.StateMachine.Tests.Tests
         [TestMethod]
         public void EnsurePropertyStateIsNotPubliclyAccessible()
         {
-            var prop = typeof(StateMachineBase<RevolverStates>).GetProperty("State", BindingFlags.Instance | BindingFlags.Public);
+            var prop = typeof(StateMachineBase<RevolverStates>).GetProperty(
+                "State",
+                BindingFlags.Instance | BindingFlags.Public
+            );
             Assert.IsNull(prop);
         }
 
         [TestMethod]
         public void EnsurePropertyStateIsProtectedAccessible()
         {
-            var prop = typeof(StateMachineBase<RevolverStates>).GetProperty("State", BindingFlags.Instance | BindingFlags.NonPublic);
+            var prop = typeof(StateMachineBase<RevolverStates>).GetProperty(
+                "State",
+                BindingFlags.Instance | BindingFlags.NonPublic
+            );
             Assert.IsNotNull(prop);
             var getter = prop.GetMethod;
             Assert.IsNotNull(getter);
